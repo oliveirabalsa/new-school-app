d372280b01e5198d89dd91a6ce5697950e42edde
oliveirabalsa <oliveirabalsa2@gmail.com>
2019-12-26 17:07:01 -0300

    about page of new school app

---
+++ todo-newschool/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+<div >
+  <v-app id="inspire">
+    <v-app-bar
+      app
+     
+      
+      
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="60"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        target="_blank"
+        text
+      >
+        <span class="mr-2"></span>
+        <v-icon>fas fa-list</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content class="back">
+      <About/>
+    </v-content>
+    <Footer/>
+  </v-app>
+  </div>
+</template>
+
+<script>
+import About from './components/About';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+
+  components: {
+    About,
+    Footer,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+
+
+#inspire{
+  background: none;
+}
+
+.back{
+  background-image: url(./assets/paraisopolis1.png); 
+  background-size: cover
+}
+</style>
---
+++ todo-newschool/src/components/Footer.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-card height="1px">
+    <v-footer
+      v-bind="localAttrs"
+      :padless="padless"
+    >
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="white lighten-1 text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4"
+            icon
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+      </v-card>
+    </v-footer>
+
+    <v-row
+      align="center"
+      justify="center"
+      class="ma-12"
+    >
+      <v-col
+        cols="12"
+        md="8"
+      >
+
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+
+<script>
+export default {
+  name: 'Footer',
+
+data: () => ({
+    icons: [
+      'mdi-home',
+      'mdi-email',
+      'mdi-calendar',
+      'mdi-delete',
+    ],
+    items: [
+      'default',
+      'absolute',
+      'fixed',
+    ],
+    padless: 'default',
+    variant: false,
+  }),
+  computed: {
+    localAttrs () {
+      const attrs = {}
+
+      if (this.variant === 'default') {
+        attrs.absolute = false
+        attrs[this.variant] = false
+      } else {
+          attrs.fixed = true
+        
+      }
+      return attrs
+    },
+  },
+}
+</script>
